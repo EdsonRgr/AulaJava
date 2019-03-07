91e92b9e8756539960b905fed115e730ec7a72c9
edsonrgr <digabum@gmail.com>
2019-03-07 10:43:51 -0300

    adicionando funções aos botões

--- ProjetoJava/src/view2/Janela.java
+++ ProjetoJava/src/view2/Janela.java
@@ -2,10 +2,14 @@ package view2;
 
 import java.awt.Container; // biblioteca para o container 
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton; // biblioteca para os botões
 import javax.swing.JFrame;
 import javax.swing.JTextField; 
+import view2.OperacoesMat;
+
 
 public class Janela extends JFrame{
 	
@@ -49,9 +53,14 @@ public class Janela extends JFrame{
 	JButton btnPont = new JButton(".");
 	JButton btnSom= new JButton("+");
 	JButton btnIgual = new JButton("=");
+	
+	OperacoesMat mat = new OperacoesMat();
+	String sinal= null;
+	double valor1 = 0, valor2 = 0 ;
+	
 
 	public Janela() {
-		super ("Nome da janela"); // o comando super descreve o nome da janela
+		super ("Calc Win7"); // o comando super descreve o nome da janela
 		Container paine = this.getContentPane();// Criando o Container 
 		paine.setLayout(null);// para nao trabalhar com nenhum layout
 		
@@ -61,91 +70,281 @@ public class Janela extends JFrame{
 		//painel 
 		paine.add(btnExibir);
 		btnExibir.setBounds(10, 2, 110, 30);
+		
 		paine.add(btnEditar);
 		btnEditar.setBounds(120, 2, 110, 30);
+		
 		paine.add(btnAjuda);
 		btnAjuda.setBounds(220,2,110,30);
+		
 		
 		
 		//primeira linha
 		paine.add(btnMC);
 		btnMC.setBounds(10 ,80 , 60, 40); // (coluna, linha,espaço,altura ) 
+		
 		paine.add(btnMR);
 		btnMR.setBounds(75 ,80 , 60, 40);
+		
 		paine.add(btnMS);
 		btnMS.setBounds(140 ,80 , 60, 40);
+		
 		paine.add(btnMmenos);
 		btnMmenos.setBounds(205 ,80 , 60, 40);
+		
 		paine.add(btnMmais);
 		btnMmais.setBounds(270 ,80 , 60, 40);
+		
 	
 		//segunda linha
 		paine.add(btnApagar);
 		btnApagar.setBounds(10, 125, 60, 40);
+		
 		paine.add(btnCE);
 		btnCE.setBounds(75, 125, 60, 40);
+		btnCE.addActionListener(new ActionListener() {   //colocando o botão para funcionar 
+			public void actionPerformed(ActionEvent e) {
+
+				txtresult.setText("0");
+				
+			}
+		});
+		
+		
 		paine.add(btnC);
 		btnC.setBounds(140 ,125 , 60, 40);
+		
 		paine.add(btnMoM);
 		btnMoM.setBounds(205,125 , 60, 40);
+		
 		paine.add(btnRQ);
 		btnRQ.setBounds(270 ,125 , 60, 40);
+		
 		
 		//terceira linha
 		paine.add(btn7);
 		btn7.setBounds(10 ,170 , 60, 40);
+		btn7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(txtresult.getText().equals("0")) {   // se o txtresult estiver com o valor 0 add 7
+					txtresult.setText("7");
+				}else {
+					txtresult.setText(txtresult.getText() + "7"); // caso ja tenho um valor add num7 
+				}
+				
+				
+			}
+		});
+		
 		paine.add(btn8);
 		btn8.setBounds(75 ,170, 60, 40);
+		btn8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(txtresult.getText().equals("0")) {
+					txtresult.setText("8");
+				}else {
+					txtresult.setText(txtresult.getText() + "8");
+				}
+				
+				
+			}
+		});
+		
+		
 		paine.add(btn9);
 		btn9.setBounds(140 ,170 , 60, 40);
+		btn9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(txtresult.getText().equals("0")) {
+					txtresult.setText("9");
+				}else {
+					txtresult.setText(txtresult.getText() + "9");
+				}
+				
+				
+			}
+		});
+		
+		
 		paine.add(btnDiv);
 		btnDiv.setBounds(205 ,170 , 60, 40);
+		
 		paine.add(btnPor);
 		btnPor.setBounds(270 ,170 , 60, 40);
+		
 		
 		//Quarta linha
 		paine.add(btn4);
 		btn4.setBounds(10 ,215 , 60, 40);
+		btn4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(txtresult.getText().equals("0")) {
+					txtresult.setText("4");
+				}else {
+					txtresult.setText(txtresult.getText() + "4");
+				}
+				
+				
+			}
+		});
+		
+		
 		paine.add(btn5);
 		btn5.setBounds(75 ,215, 60, 40);
+		btn5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(txtresult.getText().equals("0")) {
+					txtresult.setText("5");
+				}else {
+					txtresult.setText(txtresult.getText() + "5");
+				}
+				
+				
+			}
+		});
+		
+		
 		paine.add(btn6);
 		btn6.setBounds(140 ,215, 60, 40);
+		btn6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(txtresult.getText().equals("0")) {
+					txtresult.setText("6");
+				}else {
+					txtresult.setText(txtresult.getText() + "6");
+				}
+				
+				
+			}
+		});
+		
+		
 		paine.add(btnMult);
 		btnMult.setBounds(205 ,215 , 60, 40);
+		
 		paine.add(btnFrac);
 		btnFrac.setBounds(270 ,215 , 60, 40);
+		
 	
 		//Quinta linha
 		paine.add(btn1);
 		btn1.setBounds(10 ,260 , 60, 40);
+		btn1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(txtresult.getText().equals("0")) {
+					txtresult.setText("1");
+				}else {
+					txtresult.setText(txtresult.getText() + "1");
+				}
+				
+				
+			}
+		});
+		
+		
 		paine.add(btn2);
 		btn2.setBounds(75 ,260, 60, 40);
+		btn2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(txtresult.getText().equals("0")) {
+					txtresult.setText("2");
+				}else {
+					txtresult.setText(txtresult.getText() + "2");
+				}
+				
+				
+			}
+		});
+		
+		
 		paine.add(btn3);
 		btn3.setBounds(140 ,260 , 60, 40);
+		btn3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(txtresult.getText().equals("0")) {
+					txtresult.setText("3");
+				}else {
+					txtresult.setText(txtresult.getText() + "3");
+				}
+				
+				
+			}
+		});
+		
+		
 		paine.add(btnSub);
 		btnSub.setBounds(205 ,260 , 60, 40);
+		
 		
 		//Sexta linha
 		paine.add(btn0);
 		btn0.setBounds(10 ,305 , 125, 40); // (coluna, linha,espaço,altura )
+		btn0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(txtresult.getText().equals("0")) {
+					txtresult.setText("0");
+				}else {
+					txtresult.setText(txtresult.getText() + "0");
+				}
+				
+				
+			}
+		});
+		
+		
 		paine.add(btnPont);
 		btnPont.setBounds(140 ,305, 60, 40);
+		
 		paine.add(btnSom);
 		btnSom.setBounds(205 ,305 , 60, 40);
+		btnSom.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valor1 = Double.parseDouble(txtresult.getText());
+				sinal = "soma";
+				txtresult.setText("0");
+			}
+		});
+		
 		paine.add(btnIgual);
 		btnIgual.setBounds(270 ,260 , 60, 85);
+		btnIgual.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				valor2 = Double.parseDouble(txtresult.getText());
+
+				if(sinal.equals("soma")) {
+					txtresult.setText(mat.soma(valor1, valor2) + "");
+				}
+				
+			}
+		});
+		
 		
 		this.setSize(350,400); // comando que define o tamanho da janela
 		this.setVisible(true); // comando que define a visualição da janela 
 		this.setResizable(false);//comando que nao permite a alteração no tamanha da janela
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//comando que define que a janela seja encerrada
 		
-		
+		btnIgual.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				
+				
+			}
+		});
+	
 	}
-	
-	
-	
-	
 	public static void main(String[] args) {
 		Janela janela1 = new Janela();
 		
---
+++ ProjetoJava/src/view2/OperacoesMat.java
@@ -0,0 +1,19 @@
+package view2;
+
+public class OperacoesMat {
+
+public double soma (double valor1 , double valor2) {
+		return valor1+valor2;
+}
+public double mult (int mult1 , int mult2) {
+	return mult1*mult2;
+}
+public double div (int div1 , int div2) {
+	return div1/div2;
+}
+public double sub (int sub1 , int sub2) {
+	return sub1-sub2;
+}
+
+
+}
