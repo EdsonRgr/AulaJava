3234fc00bce81c228248a8e7b56d0b23454dcac0
edsonrgr <digabum@gmail.com>
2019-04-01 19:42:12 -0300

    Colocando Mascara no Formulario e etc

---
+++ AulaCadastro/src/view/PessoaFisica.java
@@ -0,0 +1,33 @@
+package view;
+
+	
+
+	public class PessoaFisica {
+		private String nome;
+		private int rg, cpf, idade ;
+
+
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		
+		public int getRg() {
+			return rg ;
+		}
+		
+		public void setRg(int rg) {
+			this.rg = rg ;
+		}
+		
+		public int getcpf() {
+			return cpf ;
+		}
+		public void setCpf(int cpf) {
+			this.cpf = cpf ;
+		}
+		
+	}
+
--- AulaCadastro/src/view/ProjetoCadastro.java
+++ AulaCadastro/src/view/ProjetoCadastro.java
@@ -1,18 +1,23 @@
 package view;
 
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JTextField;
+import javax.swing.text.MaskFormatter;
 
 public class ProjetoCadastro extends JFrame {
-
+	PessoaFisica pf = new PessoaFisica();
+	
 	JTextField txtNome = new JTextField("");
 	JLabel lblNome = new JLabel("Nome :") ;
 	
@@ -25,8 +30,11 @@ public class ProjetoCadastro extends JFrame {
 	JTextField txtBairro = new JTextField("");
 	JLabel lblBairro = new JLabel("Bairro :");
 	
+	
+	//JFormattedTextField txtTel = null ;
 	JTextField txtTel = new JTextField("");
 	JLabel lblTel = new JLabel("Telefone :");
+	
 	
 	JTextField txtRg = new JTextField("");
 	JLabel lblRg = new JLabel("RG :");
@@ -39,6 +47,8 @@ public class ProjetoCadastro extends JFrame {
 		
 		rdbsexo[0] = new JRadioButton("Masculinho");
 		rdbsexo[1] = new JRadioButton("Feminino");
+		
+		
 	}
 	
 	
@@ -53,13 +63,22 @@ public class ProjetoCadastro extends JFrame {
 	JLabel lblCel = new JLabel ("Celular :");
 	JTextField txtCel = new JTextField("");
 	
+	
 	JLabel lblCpf = new JLabel ("Cpf :");
 	JTextField txtCpf = new JTextField("");
 	
 	JButton btnSalvar = new JButton("Salvar ");
 	JButton btnImprimir = new JButton("Imprimir");
 	
+	MaskFormatter fTel = null; // formatando as mascaras
+	MaskFormatter fRg = null;
+	MaskFormatter fCep = null;
+	MaskFormatter fCel = null;
+	MaskFormatter fCpf = null;
 	
+	
+	
+	//view.PessoaFisica pessoaFisica = new view.PessoaFisica();
 	
 	
 	
@@ -92,11 +111,26 @@ public class ProjetoCadastro extends JFrame {
 		paine.add(lblBairro);
 		lblBairro.setBounds(20, 180, 80, 20);
 		
+		
+		
+		try {        
+			fTel = new MaskFormatter("(##)####-####"); //a mascara
+			txtTel = new JFormattedTextField(fTel);
+		} catch (Exception ex) { //exibir o erro
+			ex.printStackTrace();
+		}
 		paine.add(txtTel);
 		txtTel.setBounds(90, 220, 150, 40); //(coluna, linha,espaço,altura )
 		paine.add(lblTel);
 		lblTel.setBounds(20, 230, 80, 20);
 		
+		
+		try {        
+			fRg = new MaskFormatter("##.###.###-#"); //a mascara
+			txtRg = new JFormattedTextField(fRg);
+		} catch (Exception ex) { //exibir o erro
+			ex.printStackTrace();
+		}
 		paine.add(txtRg);
 		txtRg.setBounds(90, 270, 150, 40); //(coluna, linha,espaço,altura )
 		paine.add(lblRg);
@@ -112,7 +146,12 @@ public class ProjetoCadastro extends JFrame {
 		paine.add(rdbsexo[1]);
 		rdbsexo[1].setBounds(500, 20, 100, 40);
 		
-		
+		try {        
+			fCep = new MaskFormatter("#####-###"); //a mascara
+			txtCep = new JFormattedTextField(fCep);
+		} catch (Exception ex) { //exibir o erro
+			ex.printStackTrace();
+		}
 		paine.add(lblCep);
 		lblCep.setBounds(350, 70, 80, 40);
 		paine.add(txtCep);
@@ -122,34 +161,74 @@ public class ProjetoCadastro extends JFrame {
 		lblEstado.setBounds(350, 120, 80, 40);
 		paine.add(cmbEst);
 		cmbEst.setBounds(420, 120, 80, 40);
-		cmbEst.addItem("AC");		cmbEst.addItem("AL");
-		cmbEst.addItem("AP");		cmbEst.addItem("AM");
-		cmbEst.addItem("BA");		cmbEst.addItem("CE");
-		cmbEst.addItem("DF");		cmbEst.addItem("ES");
-		cmbEst.addItem("GO");		cmbEst.addItem("MA");
-		cmbEst.addItem("MT");		cmbEst.addItem("MS");
-		cmbEst.addItem("MG");		cmbEst.addItem("PA");
-		cmbEst.addItem("PB");		cmbEst.addItem("PR");
-		cmbEst.addItem("PE");		cmbEst.addItem("PI");
-		cmbEst.addItem("RJ");		cmbEst.addItem("RN");
-		cmbEst.addItem("RS");		cmbEst.addItem("RO");
-		cmbEst.addItem("RR");		cmbEst.addItem("SC");
-		cmbEst.addItem("SP");		cmbEst.addItem("SE");
+		cmbEst.addItem("AC");		
+		cmbEst.addItem("AL");
+		cmbEst.addItem("AP");		
+		cmbEst.addItem("AM");
+		cmbEst.addItem("BA");		
+		cmbEst.addItem("CE");
+		cmbEst.addItem("DF");		
+		cmbEst.addItem("ES");
+		cmbEst.addItem("GO");		
+		cmbEst.addItem("MA");
+		cmbEst.addItem("MT");		
+		cmbEst.addItem("MS");
+		cmbEst.addItem("MG");		
+		cmbEst.addItem("PA");
+		cmbEst.addItem("PB");		
+		cmbEst.addItem("PR");
+		cmbEst.addItem("PE");		
+		cmbEst.addItem("PI");
+		cmbEst.addItem("RJ");		
+		cmbEst.addItem("RN");
+		cmbEst.addItem("RS");		
+		cmbEst.addItem("RO");
+		cmbEst.addItem("RR");		
+		cmbEst.addItem("SC");
+		cmbEst.addItem("SP");		
+		cmbEst.addItem("SE");
 		cmbEst.addItem("TO");	
 		
 		
+		
+		try {        
+			fCel = new MaskFormatter("(##)#####-####"); //a mascara
+			txtCel = new JFormattedTextField(fCel);
+		} catch (Exception ex) { 
+			ex.printStackTrace();
+		}
 		paine.add(lblCel);
 		lblCel.setBounds(350, 220 , 80, 40);
 		paine.add(txtCel);
 		txtCel.setBounds(400, 220, 150, 40); //(coluna, linha,espaço,altura )
 		
+		
+		try {        
+			fCpf = new MaskFormatter("###.###.###-##"); //a mascara
+			txtCpf = new JFormattedTextField(fCpf);
+		} catch (Exception ex) { 
+			ex.printStackTrace();
+		}
 		paine.add(lblCpf);
 		lblCpf.setBounds(350, 270,80 , 40);
 		paine.add(txtCpf);
 		txtCpf.setBounds(400, 270, 150, 40); //(coluna, linha,espaço,altura )
 		
+		
+		
+		
 		paine.add(btnSalvar);
 		btnSalvar.setBounds(240, 320, 120, 40);
+		btnSalvar.addActionListener(new ActionListener() {
+			
+			
+			public void actionPerformed(ActionEvent e) {
+				ProjetoCadastro.setNome(txtNome.getText());
+				
+				
+			}
+		});
+		
 		
 		paine.add(btnImprimir);
 		btnImprimir.setBounds(370, 320, 120, 40);
